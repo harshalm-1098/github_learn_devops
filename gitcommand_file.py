db9af6a607eaee1ab6d4b300fd262ba638974562
Harshal <harshal@harshal>
2026-03-19 22:36:07 +0530

    added file

--- gitcommand_file.py
+++ gitcommand_file.py
@@ -4,3 +4,9 @@ git add .
 rm file name
 git commit -m "message"
 git restore filename
+git remote add origin https://github.com//github_learn_devops.git
+git remote -v
+ git push origin master
+ git remote set-url origin https://@github.com/github_learn_devops.git
+ git remote -v
+git push origin master
